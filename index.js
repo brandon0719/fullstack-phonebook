c1d0fe9472c6f2f08b7228fbaf91d18a29430c99
Brandon Wong <brandon.r.wong@stonybrook.edu>
2024-07-17 02:04:16 -0400

    Fixes

--- index.js
+++ index.js
@@ -137,13 +137,14 @@ app.put('/api/persons/:id', (req, res, next) => {
 })
 
 // error handler middleware
-const errorHandler = (error, req, res) => {
+const errorHandler = (error, req, res, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message })
   }
+  next(error)
 }
 
 app.use(errorHandler)
